b37d2305fe64893626fd7c90c7027038c94fab21
Baylac-Jacqué Félix <baylac.felix@gmail.com>
2012-10-24 20:11:14 +0000

    Changing emacs theme.

--- .emacs
+++ .emacs
@@ -1,3 +1,4 @@
+(setq load-path (cons "~/.emacs.d" load-path))
 (set-language-environment "UTF-8")
 ;; Affichage
 (menu-bar-mode -1)
@@ -8,8 +9,16 @@
 
 (setq truncate-partial-width-windows nil)
 (setq ring-bell-function 'ignore)
+;;(load-file "~/.emacs.d/color-themes/tangotango.el")
 
 (display-time-mode t)
 (setq custom-file "~/.emacs-custom.el")
 (load custom-file)
+(autoload 'python-mode "python-mode.el" "Python mode." t)
+(setq auto-mode-alist (append '(("/*.\.py$" . python-mode)) auto-mode-alist))
 
+(add-to-list 'load-path "~/.emacs.d/color-themes/")  
+
+(require 'color-theme) 
+(color-theme-initialize)
+(color-theme-tangotango)
--- .emacs-custom.el
+++ .emacs-custom.el
@@ -4,11 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (tango-dark)))
+ 
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 106 :width normal :foundry "unknown" :family "xos4-terminus")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 106 :width normal :foundry "unknown" :family "monaco"))))
+)
+
